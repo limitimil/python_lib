40678d8a17219b57a08b9e6589184607c1b15307
limin <zlsh10340@gmail.com>
2018-04-11 23:05:56 +0800

    fix bugs, add member functions [major] and [__getitem__]

--- PPIReader.py
+++ PPIReader.py
@@ -1,16 +1,21 @@
 from StringIO import StringIO
 import csv
 import pivotHandler as pH
-class PPIReader(pH):
+class PPIReader(pH.pivot):
     def participant(self):
-        major = self.ppi.keys()
+        major = self.piv.keys()
         partner = []
-        for v in self.ppi.values():
+        for v in self.piv.values():
             partner += v
             
         return list(set(major + partner))
+    def majors(self):
+        return self.piv.keys()
+    def __getitem__(self, key):
+        return self.piv[key]
 if __name__ == '__main__':
     import sys
-    pr = PPIReader(open(sys.argv[1]))
+    pr = PPIReader()
+    pr.read(sys.argv[1])
     print '\n'.join(map(lambda x: ','.join(x), pr.flattened()))
 
